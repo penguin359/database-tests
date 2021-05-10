80ac6dce63e4c9a408f30f36091ab7e91d821e51
Loren M. Lang <lorenl@north-winds.org>
2021-05-10 02:04:16 -0700

    Skip over week-ends and holidays

--- sql/checkout_times.sql
+++ sql/checkout_times.sql
@@ -9,6 +9,14 @@ BEGIN
 		expire_time := expire_time + '1 day'::interval;
 	END IF;
 	expire_time := expire_time + checkout_length;
+	IF extract(dow from expire_time) = 6 THEN  -- Saturday
+		expire_time := expire_time + '2 day'::interval;
+	ELSIF extract(dow from expire_time) = 0 THEN  -- Sundau
+		expire_time := expire_time + '1 day'::interval;
+	END IF;
+	WHILE expire_time::date IN (SELECT holiday FROM holiday) LOOP
+		expire_time := expire_time + '1 day'::interval;
+	END LOOP;
 END;
 $function$
 LANGUAGE plpgsql;
--- tests/01checkout.sql
+++ tests/01checkout.sql
@@ -2,16 +2,45 @@ BEGIN TRANSACTION;
 
 -- Load checkout_times SQL function
 \i sql/checkout_times.sql
+\i sql/holiday.sql
 
-SELECT plan(2);
+SELECT plan(8);
 
 -- Checked out before 4 PM on Monday for one day, expected to check in at 4 PM on Tuesday
 SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T15:23:00'::timestamptz, '1 day'::interval)),
 	  '2020-12-08T16:00:00'::timestamptz);
 
+-- Checked out after 4 PM on Monday for one day, expected to check in at 4 PM on Wednesday
+-- This ensures they get a full 24 hours for their checkout
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T16:07:00'::timestamptz, '1 day'::interval)),
+	  '2020-12-09T16:00:00'::timestamptz);
+
 -- Checked on Monday for 4 days should land on Friday
-SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T15:23:00'::timestamptz, '4 days'::interval)),
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T01:10:00'::timestamptz, '4 days'::interval)),
 	  '2020-12-11T16:00:00'::timestamptz);
+
+-- Checked on Monday for 5 days should land on week-end and be bumped to Monday
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T08:45:00'::timestamptz, '5 days'::interval)),
+	  '2020-12-14T16:00:00'::timestamptz);
+
+-- Checked on Monday for 6 days should land on week-end and be bumped to Monday
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T08:00:00'::timestamptz, '6 days'::interval)),
+	  '2020-12-14T16:00:00'::timestamptz);
+
+-- Checked on Monday for 7 days should land on Monday
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T15:23:00'::timestamptz, '7 days'::interval)),
+	  '2020-12-14T16:00:00'::timestamptz);
+
+-- If Monday is a holiday, a 5 day checkout should be extended to Tuesday
+
+INSERT INTO holiday (holiday) VALUES ('2020-12-14'::date);
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T08:45:00'::timestamptz, '5 days'::interval)),
+	  '2020-12-15T16:00:00'::timestamptz);
+
+-- If Monday and Tuesday are holidays, a 5 day checkout should be extended to Wednesday
+INSERT INTO holiday (holiday) VALUES ('2020-12-15'::date);
+SELECT is((SELECT expire_time FROM checkout_times('2020-12-07T08:45:00'::timestamptz, '5 days'::interval)),
+	  '2020-12-16T16:00:00'::timestamptz);
 
 SELECT * FROM finish();
 
